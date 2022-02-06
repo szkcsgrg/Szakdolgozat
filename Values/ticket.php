d4338ba2263b2e44d2b1f5402603d63a51741147
Szakács Gergő <szkcsgrg@gmail.com>
2022-02-06 17:11:40 +0100

    Ticket Changes

--- Values/ticket.php
+++ Values/ticket.php
@@ -7,7 +7,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Könyvtár - Elfelejtett jelszó</title>
 
-
     <!-- Srcipts -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous">
@@ -31,25 +30,21 @@
 
     <div class="container-fluid text-center">
         <div class="row justify-content-center text-center m-4">
-            <h2>Jelszó Módósítás...</h2>
-            <p>egy olyan folyamat amely biztonsági okokból csak is a könyvtárban valósítható meg.
-                Adja meg email címét a további lépések érdekében.
-            </p>
-            <div class="col-4 m-5">
-                <form method="POST">
-                    <input type='text' name='email' class='form-control m-2' placeholder='Email cím'>
-                    <input type='submit' name='submit' value='Küldés' class='btn btn-success m-2'>
-                </form>
-            </div>
+            <h1 class="m-5">Jelszó Módósítás</h1>
+            <form method="POST" class="col-4">
+                <input type='text' name='email' class='form-control m-2' placeholder='Email cím'>
+                <input type='submit' name='submit' value='Küldés' class='btn btn-success m-2'>
+            </form>
         </div>
-        <?php
-        if (isset($_POST['submit']) && isset($_POST['email'])) {
-            include_once "../Values/constans.php";
-            $today = date("Y.m.d.");
-            $email = $_POST['email'];
-            $conn->query("INSERT INTO `konyvtar`.`tickets` (`emailID`, `datum`) VALUES ('" . $email . "', '" . $today . "');");
-            echo "Jelszó módósítási kérését mentettük. <br> A következő lépés: fáradjon be a könyvtárunkba!";
-        }
-        ?>
+    </div>
+    <?php
+    if (isset($_POST['submit']) && isset($_POST['email'])) {
+        include_once "../Values/constans.php";
+        $today = date("Y.m.d.");
+        $email = $_POST['email'];
+        $conn->query("INSERT INTO `konyvtar`.`tickets` (`emailID`, `datum`) VALUES ('" . $email . "', '" . $today . "');");
+        echo "Jelszó módósítási kérését mentettük. <br> A következő lépés: fáradjon be a könyvtárunkba!";
+    }
+    ?>
     </div>
 </body>
